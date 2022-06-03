0b2b2c18cf66213a75f25e0d0b68008b6cf732ff
Asianguy123 <sas@drsyed.org>
2022-06-03 01:13:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Javascript File for School Web Tasks 1.3.4
+
+function setStyleSheet(url) {
+    // Changes stylesheet of index.html, using id
+    
+    var stylesheet = document.getElementById('stylesheet');
+    stylesheet.setAttribute('href', url);
+}
